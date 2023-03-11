4db0085ed6f0fb46e70d9dfad857c548cacf00b5
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-03-10 23:22:20 -0400

    configurando express

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Holaaaa');
+const express = require('express');
+
+
+const app = express();
+
+
+app.get('/',(req, res)=>{
+    console.log('se requiere /');
+    res.json({
+        ok:true
+    })
+})
+
+app.listen( 3001,() =>{
+    console.log(`Servidor corriendo en puerto ${ 4000 }`);
+});
