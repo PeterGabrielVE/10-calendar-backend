f0ef0a49b996932b8b69866254cfab895ddc3597
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-03-12 11:26:43 -0400

    puerto configuraciones

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express');
-
+require('dotenv').config();
 
 const app = express();
 
+app.use( express.static('public') );
 
-app.get('/',(req, res)=>{
+/*app.get('/',(req, res)=>{
     console.log('se requiere /');
     res.json({
         ok:true
     })
-})
+})*/
 
-app.listen( 3001,() =>{
-    console.log(`Servidor corriendo en puerto ${ 4000 }`);
+app.listen( process.env.PORT,() =>{
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
 });
