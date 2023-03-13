4491b34c98f70a780e907d3074ac3978cb092858
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-03-12 20:47:51 -0400

    recuperar informacion de un posteo

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,17 +3,32 @@ const { response } = require('express');
 
 const crearUsuario = (req, res = express.response )=>{
 
+    const { name, email, password } = req.body;
+
+    if( name.length < 5 ){
+        return res.status(400).json({
+            ok:false,
+            msg:'El nombre debe de ser de 5 letras'
+        })
+    }
     res.json({
         ok:true,
-        msg: 'registro'
+        msg: 'registro',
+        name,
+        email,
+        password
     })
 }
 
 const loginUsuario = (req, res = express.response )=>{
 
+    const { email, password } = req.body;
+    console.log( req );
     res.json({
         ok:true,
-        msg: 'login'
+        msg: 'login',
+        email,
+        password
     })
 }
 
