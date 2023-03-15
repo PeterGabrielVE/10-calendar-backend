4b88b248dcf9c32cdf426725c00856ecac29e720
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-03-15 19:24:08 -0400

    validacioes

--- routes/events.js
+++ routes/events.js
@@ -1,6 +1,11 @@
 const { Router } = require('express');
 const { validarJWT } = require('../middlewares/validar-jwt');
 const { getEvent, createEvent, updateEvent, deleteEvent } =  require('../controllers/events');
+const { check } = require('express-validator');
+
+const { isDate } = require('../helpers/isDate');
+const { validarCampos } = require('../middlewares/validar_campos');
+
 const router = Router();
 
 router.use( validarJWT );
@@ -9,7 +14,13 @@ router.use( validarJWT );
 router.get('/', getEvent);
 
 // create eventos
-router.post('/createEvent', createEvent);
+router.post('/createEvent',
+[
+    check('title','El titulo es obligatorio').not().isEmpty(),
+    check('start','Fecha de inicio es obligatoria').custom( isDate ),
+    check('end','Fecha de finalización es obligatoria').custom( isDate ),
+    validarCampos
+],createEvent);
 
 // update eventos
 router.put('/updateEvent/:id', updateEvent);
