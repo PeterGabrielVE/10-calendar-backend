34bf3b64664f73291eeaf0ad8ae1ffb546e96f6d
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-03-15 20:25:14 -0400

    delete

--- controllers/events.js
+++ controllers/events.js
@@ -94,11 +94,40 @@ const updateEvent = async(req, res = express.response )=>{
 
 const deleteEvent = async(req, res = express.response )=>{
 
+    const eventId = req.params.id;
+    const uid = req.uid;
 
-    res.json({
-        ok:true,
-        msg: 'Delete Event'
-    });
+    try {
+
+        const event = await Event.findById( eventId );
+
+        if ( !event ) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Evento no existe por ese id'
+            });
+        }
+
+        if ( event.user.toString() !== uid ) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegio de eliminar este evento'
+            });
+        }
+
+
+        await Event.findByIdAndDelete( eventId );
+
+        res.json({ ok: true });
+
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+    }
 }
 
 module.exports ={
